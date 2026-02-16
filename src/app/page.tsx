d0967fad57a8271423a86575dca9fb206cad2f4e
lucasfgaldinos <lucasfgaldinos@outlook.com>
2026-02-16 11:11:47 -0300

    feat: Cria a seção de Nossos Serviços.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { About } from "@/sections/about";
 import { Benefits } from "@/sections/benefits";
 import { Hero } from "@/sections/hero";
 import { MissionAndValues } from "@/sections/mission-and-values";
+import { Services } from "@/sections/services";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         <About />
         <MissionAndValues />
         <Benefits />
+        <Services />
       </main>
     </div>
   );
---
+++ src/sections/services.tsx
@@ -0,0 +1,82 @@
+"use client";
+import {
+  BriefcaseIcon,
+  HeadsetIcon,
+  HouseLineIcon,
+} from "@phosphor-icons/react";
+import { InfoCard } from "@/components/info-card";
+import { Section } from "@/components/section";
+import { Title } from "@/components/title";
+
+export function Services() {
+  return (
+    <Section>
+      <Title>Nossos Serviços</Title>
+
+      <div className="flex flex-col lg:flex-row gap-4 md:gap-8 mt-10">
+        {/* Primeiro card */}
+        <InfoCard
+          name={{
+            alignment: "center",
+            content: "Instalação Residencial",
+            size: "md",
+          }}
+          description={{
+            alignment: "center",
+            content:
+              "Projetos personalizados para sua casa, garantindo máxima eficiência e economia na sua conta de luz.",
+            size: "md",
+          }}
+          bg="secondary"
+          icon={{
+            bg: false,
+            alignment: "center",
+            content: <HouseLineIcon />,
+          }}
+        />
+
+        {/* Segundo card */}
+        <InfoCard
+          name={{
+            alignment: "center",
+            content: "Instalação Comercial",
+            size: "md",
+          }}
+          description={{
+            alignment: "center",
+            content:
+              "Soluções para empresas de todos os tamanhos que buscam reduzir custos operacionais e ser mais sutentáveis.",
+            size: "md",
+          }}
+          bg="secondary"
+          icon={{
+            bg: false,
+            alignment: "center",
+            content: <BriefcaseIcon />,
+          }}
+        />
+
+        {/* Terceiro card */}
+        <InfoCard
+          name={{
+            alignment: "center",
+            content: "Manutenção e Suporte",
+            size: "md",
+          }}
+          description={{
+            alignment: "center",
+            content:
+              "Garantimos o desempenho contínuo de seu sistema com nossa equipe de especialistas sempre a postos.",
+            size: "md",
+          }}
+          bg="secondary"
+          icon={{
+            bg: false,
+            alignment: "center",
+            content: <HeadsetIcon />,
+          }}
+        />
+      </div>
+    </Section>
+  );
+}
